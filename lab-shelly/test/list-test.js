63ebca5ca4cf44ba6570454c5326d42c636f568a
Shelly Tang <shellytang@gmail.com>
2017-04-28 12:12:16 -0700

    more tests

--- lab-shelly/test/list-test.js
+++ lab-shelly/test/list-test.js
@@ -7,13 +7,10 @@ let list = new List(1,2,3,4);
 
 describe('testing list module', function() {
 
-
   describe('testing map method', function() {
-
     let testArr = list.map(function(a) {
       return a + 1;
     });
-
     it('should return a new array', function() {
       expect(testArr).to.be.a('array');
     });
@@ -36,4 +33,20 @@ describe('testing list module', function() {
     });
 
   });
+
+  describe('testing forEach method', function() {
+    let testArr = list.forEach(function(a) {
+      return a + 1;
+    });
+    it('should return a new array', function() {
+      expect(testArr).to.be.a('array');
+    });
+    // it('should return a new array with same length of original array', function() {
+    //   expect(testArr).to.have.length.of(4);
+    // });
+    // it('should have a callback as an argument that adds 1 to the index value', function() {
+    //   expect(testArr[1]).to.be.equal(3);
+    // });
+  });
+
 });
